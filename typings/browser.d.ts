beae7bedeee10eab13abbf0a90292d5021c29376
Jonathan Glanz <jonathan.glanz@gmail.com>
2016-05-20 23:30:50 -0400

    Separated from typedux

---
+++ typings/browser.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="browser/ambient/chai/index.d.ts" />
+/// <reference path="browser/ambient/mocha/index.d.ts" />
+/// <reference path="browser/ambient/node/index.d.ts" />
+/// <reference path="browser/definitions/sinon/index.d.ts" />
+/// <reference path="browser/definitions/reflect-metadata/index.d.ts" />
---
+++ typings/main.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="main/ambient/chai/index.d.ts" />
+/// <reference path="main/ambient/mocha/index.d.ts" />
+/// <reference path="main/ambient/node/index.d.ts" />
+/// <reference path="main/definitions/sinon/index.d.ts" />
+/// <reference path="browser/definitions/reflect-metadata/index.d.ts" />
