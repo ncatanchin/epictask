9773c843d6cccec0a6950cf291f834887257a820
Jonathan Glanz <jonathan.glanz@gmail.com>
2016-06-21 20:47:07 -0400

    - removed sqlite postinstall comp

--- etc/scripts/post-install.sh
+++ etc/scripts/post-install.sh
@@ -5,6 +5,6 @@ pushd node_modules/leveldown
 node-gyp rebuild --target=1.2.3 --arch=x64 --dist-url=https://atom.io/download/atom-shell
 popd
 
-pushd node_modules/sqlite3
-node-gyp rebuild --target=1.2.3 --arch=x64 --dist-url=https://atom.io/download/atom-shell
-popd
+#pushd node_modules/sqlite3
+#node-gyp rebuild --target=1.2.3 --arch=x64 --dist-url=https://atom.io/download/atom-shell
+#popd
