24421213b1d453f76b2920827f2537f98a5fd98f
Jonathan Glanz <jonathan.glanz@gmail.com>
2016-07-18 20:42:33 -0400

    removed ensure

--- src/ui/UIEntry.ts
+++ src/ui/UIEntry.ts
@@ -4,12 +4,12 @@ require('shared/LogConfig')
 import 'shared/RendererLogging'
 import 'shared/PromiseConfig'
 
-require.ensure(['material-ui','material-ui/svg-icons'],(require) => {
-	// Load all global/env stuff first
-	// LOGGING CONFIG FIRST
-	require('./UIGlobals')
-	require('./UIConfigurator')
-})
+//require.ensure(['material-ui','material-ui/svg-icons'],(require) => {
+// Load all global/env stuff first
+// LOGGING CONFIG FIRST
+require('./UIGlobals')
+require('./UIConfigurator')
+//})
 
 
 export {
