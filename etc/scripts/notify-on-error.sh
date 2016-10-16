25c3d38e0d5e0467d3285be5d1818c6eafffe4a4
Jonathan Glanz <jonathan.glanz@gmail.com>
2016-10-16 04:44:55 -0400

    removed unused scripts

--- etc/scripts/notify-on-error.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Looking for errors"
-pkill -9 notify-on-error
-touch logs/compile.log
-tail -f logs/compile.log | grep --line-buffered -i error | xargs -n4 terminal-notifier -sound Funk
-#
-echo "Stopped watching for errors"
