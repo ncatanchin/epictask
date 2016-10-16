dc7420c476ed26f70aa6a698f8a6516617957823
Jonathan Glanz <jonathan.glanz@gmail.com>
2016-10-16 18:39:37 -0400

    fixed spread artifact publishing

--- etc/scripts/publish-artifacts.js
+++ etc/scripts/publish-artifacts.js
@@ -16,8 +16,7 @@ const
 		EPICTASK_VERSION:version
 	} = process.env
 
-if (!(!isEmpty(accessKeyId) && !isEmpty(version) && !isEmpty(buildNumber)))
-	process.exit(0)
+
 
 async function upload() {
 	
@@ -40,43 +39,49 @@ async function upload() {
 	cd('dist/build')
 	
 	let
-		files = []
+		files = [],
+		promises = [],
+		basePath = pwd()
 	
 	for (let buildRoot of ['','mac/']) {
+		const newRoot = `${basePath}${buildRoot}`
+		echo(`Publishing from ${newRoot}`)
+		cd(newRoot)
 		for (let filePath of ["*.dmg", "*.zip", "*.exe", "*.deb", "*.AppImage"]) {
-			files = files.concat(await glob(`${buildRoot}${filePath}`))
+			files = files.concat(await glob(filePath))
 		}
+		
+		files = files.filter(file => !fs.statSync(file).isDirectory())
+		
+		echo(`Uploading ${files.length} files`)
+		
+		promises.push(...files.map(async(file, index) => {
+				echo(`Uploading ${index} of ${files.length}: ${file}`)
+				
+				try {
+					const
+						stream = fs.createReadStream(file)
+					
+					await s3.putObject({
+						Bucket,
+						Key: `electron/${file}`,
+						Body: stream,
+						ACL: 'public-read'
+					}).promise()
+					
+					echo(`Uploaded ${index} of ${files.length},${file}`)
+				} catch (err) {
+					console.error(`Failed to upload ${file}`,err)
+				}
+			}))
 	}
-	files = files.filter(file => !fs.statSync(file).isDirectory())
 	
-	echo(`Uploading ${files.length} files`)
-	
-	const
-		promises = files.map(async(file, index) => {
-			echo(`Uploading ${index} of ${files.length}: ${file}`)
-			
-			try {
-				const
-					stream = fs.createReadStream(file)
-				
-				await s3.putObject({
-					Bucket,
-					Key: `electron/${file}`,
-					Body: stream,
-					ACL: 'public-read'
-				}).promise()
-				
-				echo(`Uploaded ${index} of ${files.length},${file}`)
-			} catch (err) {
-				console.error(`Failed to upload ${file}`,err)
-			}
-		})
 	
 	await Promise.all(promises)
 	
 	echo(`All files uploaded`)
 	
-	
 }
 
-upload()
+if (!isEmpty(accessKeyId) && !isEmpty(version) && !isEmpty(buildNumber))
+	upload()
