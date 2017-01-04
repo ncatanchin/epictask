7e99b372795db432f689463694f2298a8e213198
jglanz <jonathan.glanz@gmail.com>
2017-01-04 10:04:43 -0500

    ++ enabled beta/prod update/release manager bumping version

--- packages/epic-entry-main/AppUpdateManager.ts
+++ packages/epic-entry-main/AppUpdateManager.ts
@@ -9,8 +9,8 @@ const
 	isMac = process.platform === 'darwin',
 	isLinux = !isWindows && !isMac,
 	platform = isWindows ? 'windows' : isMac ? 'macos' : 'linux',
-	baseUrl = "http://localhost:3000",
-	//baseUrl = "https://epictask.densebrain.com",
+	//baseUrl = "http://localhost:3000",
+	baseUrl = "https://epictask.densebrain.com",
 	feedUrl = `${baseUrl}/update/electron/${platform}/${VERSION}`
 
 // DEBUG
@@ -56,11 +56,11 @@ autoUpdater.checkForUpdates()
 function askUpdate(name,updateUrl,notes,releaseDate) {
 	dialog.showMessageBox(null,{
 		type: 'question',
-		buttons: ['Later', 'Update Now'],
+		buttons: ['I\'ll Restart Later', 'Restart Now'],
 		cancelId: 0,
 		defaultId: 1,
-		message: `New update available, install?`,
-		detail: notes
+		message: `Downloaded update to version ${name}, restart now or later at your convience`,
+		detail: _.isEmpty(notes) ? null : notes
 	},index => {
 		if (index === 0)
 			return
