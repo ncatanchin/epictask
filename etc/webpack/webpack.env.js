f61b78651cd3d332179cdbca6211c649f8937674
Jonathan Glanz <jonathan.glanz@gmail.com>
2019-02-12 20:01:36 -0500

    - fixed env

--- etc/webpack/webpack.env.js
+++ etc/webpack/webpack.env.js
@@ -14,6 +14,7 @@ const
   pkgJson = JSON.parse(Fs.readFileSync(Path.resolve(baseDir, 'package.json'), 'utf-8')),
   isProd = envName === 'prod'
 
+// noinspection WebpackConfigHighlighting
 module.exports = isProcessMain => ({
   isDev,
   __DEV__: isDev,
@@ -23,15 +24,8 @@ module.exports = isProcessMain => ({
   MAPPER_ENV: JSON.stringify(envName),
 
   'process.env.devToolsOpen': devToolsOpen,
-  'process.env.MAPPER_ENV': JSON.stringify(envName),
   'process.env.__DEV__': isDev,
   'process.env.PACKAGE': JSON.stringify(process.env.PACKAGE ? "true" : "false"),
   'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
   'process.env.BASEDIR': JSON.stringify(baseDir),
-  'process.env.MAPPER_CONFIG': JSON.stringify({}),
-  'process.env.MAPPER_BUILD': JSON.stringify(envStartupBuild),  // controls the startup URL in main/index.ts
-  'process.env.PUSHER_APP_ID': JSON.stringify(isProd ? '587311' : '587309'),
-  'process.env.PUSHER_KEY': JSON.stringify(isProd ? '4e9865dd5382debb146f' : 'feb85881f22a0444fc7d'),
-  'process.env.PUSHER_CLUSTER': JSON.stringify('mt1'),
-  MAPPER_BUILD: JSON.stringify(envStartupBuild)
 })
